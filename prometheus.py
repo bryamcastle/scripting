b9b5c67bafeca0e5eb63299a33808ea519ae1234
bryamcastle <bryam.castillo.net@gmail.com>
2020-04-25 06:27:44 -0500

    Update prometheus.py

--- prometheus.py
+++ prometheus.py
@@ -1,12 +1,33 @@
-from prometheus_client.core import CollectorRegistry
-from prometheus_client import Summary, Counter, Histogram, Gauge
+from prometheus_client import start_http_server, Histogram, CollectorRegistry, write_to_textfile
+import json
 import time
 
+
+start_time = time.time()
 _INF = float("inf")
+file_log = 'avantica.log'
 
-graphs = {}
-graphs['h1'] = Histogram('Tx', 'Total of number of Tx.', buckets=(500, 1000, 5000, 9000, _INF))
-graphs['h2'] = Histogram('Rx', 'Total of number of Rx.', buckets=(500, 1000, 5000, 9000, _INF))
 
-open('C:/Users/bryam/PycharmProjects/Tutorial/venv/avantica.log','r')
+class JsonData(object):
+    def __init__(self, data):
+        self.__dict__ = json.loads(data)
 
+
+def process_request():
+    registry = CollectorRegistry()
+    graphs = {}
+    graphs['h1'] = Histogram('Tx', 'Numbers of Tx.', buckets=(500, 1000, 5000, 9000, _INF), registry=registry)
+    graphs['h2'] = Histogram('Rx', 'Number of Rx.', buckets=(500, 1000, 5000, 9000, _INF), registry=registry)
+    with open(file_log, 'r') as f:
+        for i in f:
+            line = JsonData(i)
+            tx = line.Tx
+            rx = line.Rx
+            graphs['h1'].observe(tx)
+            graphs['h2'].observe(rx)
+    write_to_textfile('histogram.prom', registry)
+
+
+if __name__ == '__main__':
+    process_request()
+    print "Execution time:", (time.time() - start_time)
